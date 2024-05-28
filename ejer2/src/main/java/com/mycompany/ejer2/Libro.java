6c59d6bee0095893648f0e73544bf54b22424bb5
Ixcielos <estebannarvez@gmail.com>
2024-05-28 14:07:16 -0500

    carga inical

---
+++ ejer2/src/main/java/com/mycompany/ejer2/Libro.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejer2;
+
+/**
+ *
+ * @author felix
+ */
+public class Libro {
+    //atributos
+    public String titulo;
+    public String autor;
+    public int paginas;
+    
+    //constructor
+    public Libro(String titulo, String autor, int paginas) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.paginas = paginas;
+    }
+    
+    //metodos
+    public void abrir(){
+        System.out.println("El libro se encuentra abierto");
+    }
+    
+    public void leer(){
+        System.out.println("El libro esta siendo leido");
+    }
+    public void cerrar(){
+        System.out.println("El libro esta cerrado");
+    }
+}
